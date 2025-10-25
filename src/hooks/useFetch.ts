a049aeb12a451645c38435adf9280606b4aa1964
developerrahul27 <rahulrawattw@gmail.com>
2025-10-25 21:09:52 +0530

    Change BASE_URL to a static API endpoint

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { ApiResponse } from "../types";
 
-const BASE_URL = import.meta.env.VITE_RANDOM_USER_API;
+const BASE_URL = "https://randomuser.me/api";
 
 export const fetchUserApi = async (): Promise<ApiResponse> => {
   const { data } = await axios.get<ApiResponse>(BASE_URL);
